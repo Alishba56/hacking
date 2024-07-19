e4b7269f2c69c48684dcbd2e2b7162a2df65f033
Alishba56 <164113933+Alishba56@users.noreply.github.com>
2024-07-19 11:03:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let Hacking = function(){
+    setTimeout(()=>{
+        h1.innerHTML = "Fetching WiFi for hacking instagram... Please wait..."
+    },3000)
+    setTimeout(()=>{
+        h2.innerHTML = "Fetched WiFi getting instagram... Please wait..."
+    },5000)
+    setTimeout(()=>{
+        h3.innerHTML = "Your instagram id is Alishba Ali "
+
+    },6000)
+    setTimeout(() => {
+        h4.innerHTML= "Email = alishba_22_ali "
+    }, 7000);
+    setTimeout(() => {
+        h5.innerHTML= "password = ************ "
+    }, 7000);
+    setTimeout(()=>{
+        h6.innerHTML = "Alishba Ali your instagram id is hacked.."
+    }, 10000)
+}
+
+
+
+Hacking()
